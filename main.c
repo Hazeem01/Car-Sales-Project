eecf75d599a6478fc23d9f82ba4e4c4e6f13fc77
Hazeem <omotayo4877@gmail.com>
2024-05-29 09:42:07 +0100

    modified code and added new features

--- main.c
+++ main.c
@@ -6,12 +6,13 @@
 #define DISCOUNT_MIN_AGE 18
 #define DISCOUNT_MAX_AGE 25
 #define DISCOUNT_PERCENTAGE 0.10f
-#define MAX_SALES 50
-#define MAX_FEEDBACK 50
+#define MAX_SALES 100
+#define MAX_STOCK 3
 #define CSV_FILE "data.csv"
 #define MENU_OPTION_BUY_CARS 'a'
 #define MENU_OPTION_VIEW_SALES 'b'
 #define MENU_OPTION_FILTER_SALES_STAT 'c'
+#define MENU_View_Available_CARS 'd'
 #define MENU_OPTION_EXIT_APPLICATION 'x'
 #define FILE_OPENED 0
 #define FILE_CLOSED 1
@@ -19,18 +20,22 @@
 
 unsigned short carsAvailable = 60;
 unsigned short numberOfSales = 0;
-unsigned short numberOfFeedbacks = 0;
-unsigned short numberOfCarsPerSale[MAX_SALES];
-unsigned short carModelPerSale[MAX_SALES];
 unsigned char fileStatus = FILE_CLOSED;
 
-bool discountGivenPerSale[MAX_SALES];
+typedef struct {
+    char customerNames[201];
+    char customerFeedbacks[201];
+    unsigned short numberOfCarsPerSale;
+    unsigned short carModelPerSale;
+    bool discountGivenPerSale;
+} Sale;
 
-char customerNames[MAX_SALES][201];
-char customerFeedbacks[MAX_FEEDBACK][201];
-char carModels[][20] = {"Toyota Corolla", "Tesla Model X", "Honda Civic"};
+Sale sales[MAX_SALES];
 
-float carPrices[] = {4.0f, 8.5f, 3.5f};
+char carModels[][20] = {"Honda Civic", "Toyota Corolla", "Toyota Prius", "Tesla Model X"};
+char carYearOfManufacture[][20] = {"2020", "2021","2022", "2023"};
+float carPrices[] = {3.5f, 4.0f, 3.4f, 8.5f};
+int numberOfModelsInStock[] = {30,10, 15, 5};
 
 FILE *file;
 
@@ -160,36 +165,36 @@ void readDataFromFile()
 {
     int lineCounter = 0;
 
-    while (1)
-    {
-
-        unsigned short numberOfCarsPerSaleValue = 0, carModelPerSaleValue = 0, discountGivenPerSaleValue = 0;
-        char customerNameValue[201];
-        char customerFeedbackValue[201];
-
-        int scanResult = fscanf(
-                file,
-                "%hd,%hd,%hd,%[^\n],%[^\n]",
-                &numberOfCarsPerSaleValue,
-                &carModelPerSaleValue,
-                &discountGivenPerSaleValue,
-                customerNameValue, customerFeedbackValue);
-
-        if (scanResult == EOF)
+        while (1)
         {
-            break;
+
+            unsigned short numberOfCarsPerSaleValue = 0, carModelPerSaleValue = 0, discountGivenPerSaleValue = 0;
+            char customerNameValue[201];
+            char customerFeedbackValue[201];
+
+            int scanResult = fscanf(
+                    file,
+                    "%hd,%hd,%hd,%[^\n],%[^\n]",
+                    &numberOfCarsPerSaleValue,
+                    &carModelPerSaleValue,
+                    &discountGivenPerSaleValue,
+                    customerNameValue, customerFeedbackValue);
+
+            if (scanResult == EOF)
+            {
+                break;
+            }
+
+            sales[lineCounter].numberOfCarsPerSale = numberOfCarsPerSaleValue;
+            sales[lineCounter].carModelPerSale = carModelPerSaleValue;
+            sales[lineCounter].discountGivenPerSale = (bool)discountGivenPerSaleValue;
+            strcpy(sales[lineCounter].customerNames, customerNameValue);
+            strcpy(sales[lineCounter].customerFeedbacks, customerFeedbackValue);
+
+            lineCounter++;
         }
 
-        numberOfCarsPerSale[lineCounter] = numberOfCarsPerSaleValue;
-        carModelPerSale[lineCounter] = carModelPerSaleValue;
-        discountGivenPerSale[lineCounter] = (bool)discountGivenPerSaleValue;
-        strcpy(customerNames[lineCounter], customerNameValue);
-        strcpy(customerFeedbacks[lineCounter], customerFeedbackValue);
-
-        lineCounter++;
-    }
-
-    numberOfSales = lineCounter;
+        numberOfSales = lineCounter;
 }
 
 void getDataFromFile()
@@ -211,16 +216,16 @@ void getDataFromFile()
 
 void writeDataToFile()
 {
-    for (int i = 0; i < numberOfSales; i++)
-    {
-        fprintf(file, "%hd,%hd,%hd,%s,%s\n",
-                numberOfCarsPerSale[i],
-                carModelPerSale[i],
-                discountGivenPerSale[i],
-                customerNames[i],
-                customerFeedbacks[i]);
+        for (int i = 0; i < numberOfSales; i++)
+        {
+            fprintf(file, "%hd,%hd,%hd,%s,%s\n",
+                    sales[i].numberOfCarsPerSale,
+                    sales[i].carModelPerSale,
+                    sales[i].discountGivenPerSale,
+                    sales[i].customerNames,
+                    sales[i].customerFeedbacks);
+        }
     }
-}
 
 void saveDataToFile()
 {
@@ -250,6 +255,7 @@ void showMenu()
     printf("%c. Buy Car\n", MENU_OPTION_BUY_CARS);
     printf("%c. View Sales Stats\n", MENU_OPTION_VIEW_SALES);
     printf("%c. Filter Through Sales Stats\n", MENU_OPTION_FILTER_SALES_STAT);
+    printf("%c. View Cars Available For Sale\n", MENU_View_Available_CARS);
     printf("%c. Exit\n\n", MENU_OPTION_EXIT_APPLICATION);
 }
 
@@ -257,12 +263,16 @@ void menu_showCarModels()
 {
     int numberOfCars = sizeof(carPrices) / sizeof(float);
 
-    printf("\nCar Models:\n");
+    printf("\n              <<<Available Car Models>>>\n");
+    printf("--------------------------------------------------------\n");
+    printf("N/A    Model Name      Year Of Manufacture  Availability\n");
 
     for (int i = 0; i < numberOfCars; i++)
     {
-        printf("%d - %s\n", i, carModels[i]);
+        printf("%-3d    %-15s  %-20s  %-d    \n", i, carModels[i], carYearOfManufacture[i], numberOfModelsInStock[i]);
     }
+
+    printf("--------------------------------------------------------\n");
 }
 
 float menu_applyDiscount(float currentPrice)
@@ -291,7 +301,7 @@ void menu_printDiscountOutcome(bool giveDiscount)
     }
 }
 
-void menu_give_Feedback(char *currentCustomerName)
+void menu_give_Feedback()
 {
     clearScreen();
     char userFeedback[50];
@@ -301,10 +311,26 @@ void menu_give_Feedback(char *currentCustomerName)
     printf("\nYour Feedbacks Makes Us Better :) \n");
 
     getStringFromConsole("\nGive your feedback below...", userFeedback);
-    strcpy(customerNames[numberOfSales], currentCustomerName);
-    strcpy(customerFeedbacks[numberOfSales], userFeedback);
-    numberOfFeedbacks ++;
+    strcpy(sales[numberOfSales - 1].customerFeedbacks, userFeedback);
     printf("Thank you for your feedback!\nWe have received your feedback...");
+}
+
+void menu_viewStock()
+{
+    clearScreen();
+    printf("<---Buy Car--->\n\n");
+
+    if (carsAvailable > 0)
+    {
+        printf("We currently have a total of %hd cars available.\n\n", carsAvailable);
+    }
+    else
+    {
+        printf("Sorry, there is no car available in the store at this time");
+        return;
+    }
+
+    menu_showCarModels();
 }
 
 void menu_buyCars()
@@ -330,17 +356,18 @@ void menu_buyCars()
 
     getStringFromConsole("What is your name?", customerName);
 
-    carsNeeded = getUnsignedShortFromConsole("\nHow many cars would you be purchasing today ?");
-
-    if (carsAvailable < carsNeeded)
-    {
-        printf("Sorry, but we only have %hd cars available.\n\n", carsAvailable);
-        return;
-    }
-
     menu_showCarModels();
 
     carModel = getUnsignedShortFromConsole("\nPlease provide the car model you want from the list above");
+
+    carsNeeded = getUnsignedShortFromConsole("\nHow many cars would you be purchasing today ?");
+
+    if (numberOfModelsInStock[carModel] < carsNeeded)
+    {
+        printf("Sorry, but we only have %hd cars of this model in stock.\n\n", numberOfModelsInStock[carModel]);
+        return;
+    }
+
 
     totalPrice = carsNeeded * carPrices[carModel];
 
@@ -366,7 +393,7 @@ void menu_buyCars()
     printf("Total Cost Of Purchase: %f GBP (after any discount) | Model Of Car Purchased: %s | "
            "Unitary Cost Of Car: %f GBP | Number Of Cars Purchased: %hd | "
            "Discount Applicable: %s\n",
-           totalPrice, carModels[carModel],
+            totalPrice, carModels[carModel],
            carPrices[carModel], carsNeeded,
            discountStatus);
 
@@ -376,15 +403,18 @@ void menu_buyCars()
         proceedWithTransactionResponse = getCharFromConsole("\nInput \'Y\' to continue with this purchase \nInput \'N\' to cancel this transaction ");
         if (proceedWithTransactionResponse == 'Y') {
 
-            strcpy(customerNames[numberOfSales], customerName);
+            strcpy(sales[numberOfSales].customerNames, customerName);
 
-            numberOfCarsPerSale[numberOfSales] = carsNeeded;
+            sales[numberOfSales].numberOfCarsPerSale = carsNeeded;
 
-            carModelPerSale[numberOfSales] = carModel;
+            sales[numberOfSales].carModelPerSale = carModel;
 
-            discountGivenPerSale[numberOfSales] = giveDiscount;
+            sales[numberOfSales].discountGivenPerSale = giveDiscount;
+
+            strcpy(sales[numberOfSales].customerFeedbacks, "");
 
             carsAvailable -= carsNeeded;
+            numberOfModelsInStock[carModel] -= carsNeeded;
 
             printf("\nYou have bought a total of %hd cars costing %f GBP.\n", carsNeeded, totalPrice);
             printf("We now have %hd cars left in the store\n\n", carsAvailable);
@@ -397,7 +427,7 @@ void menu_buyCars()
             while (true) {
                 provideFeedbackResponse = getCharFromConsole("\nInput \'Y\' to give feedback \nInput \'N\' to return to the main menu ");
                 if (provideFeedbackResponse == 'Y') {
-                    menu_give_Feedback(customerName);
+                    menu_give_Feedback();
                     printf("Thanks for your purchase! Have a great day!\n");
                     return;
                 }
@@ -429,18 +459,18 @@ void sortArraysByNumberOfCarsSoldPerSale()
         for (int j = i + 1; j < numberOfSales; j++)
         {
 
-            if (numberOfCarsPerSale[i] > numberOfCarsPerSale[j])
+            if (sales[i].numberOfCarsPerSale > sales[j].numberOfCarsPerSale)
             {
 
-                swapUnsignedShort(&numberOfCarsPerSale[i], &numberOfCarsPerSale[j]);
-                swapUnsignedShort(&carModelPerSale[i], &carModelPerSale[j]);
+                swapUnsignedShort(&sales[i].numberOfCarsPerSale, &sales[j].numberOfCarsPerSale);
+                swapUnsignedShort(&sales[i].carModelPerSale, &sales[j].carModelPerSale);
 
-                swapBool(&discountGivenPerSale[i], &discountGivenPerSale[j]);
+                swapBool(&sales[i].discountGivenPerSale, &sales[j].discountGivenPerSale);
 
                 char temp[201];
-                strcpy(temp, customerNames[i]);
-                strcpy(customerNames[i], customerNames[j]);
-                strcpy(customerNames[j], temp);
+                strcpy(temp, sales[i].customerNames);
+                strcpy(sales[i].customerNames, sales[j].customerNames);
+                strcpy(sales[j].customerNames, temp);
 
             }
 
@@ -452,11 +482,11 @@ void sortArraysByNumberOfCarsSoldPerSale()
 void printSalesDataAtPosition(int position)
 {
 
-    int modelOfCar = carModelPerSale[position];
-    float price = numberOfCarsPerSale[position] * carPrices[modelOfCar];
+    int modelOfCar = sales[position].carModelPerSale;
+    float price = sales[position].numberOfCarsPerSale * carPrices[modelOfCar];
 
     char discountGivenText[4];
-    if (discountGivenPerSale[position] == true)
+    if (sales[position].discountGivenPerSale == true)
     {
         strcpy(discountGivenText, "Yes");
         price *= (1 - DISCOUNT_PERCENTAGE);
@@ -466,12 +496,13 @@ void printSalesDataAtPosition(int position)
         strcpy(discountGivenText, "No");
     }
 
-    printf("Sale Index: %d | Final Sale Amount (after any discount): %f | Car Model: %s | "
-           "Unitary Price of Car: %f | Number of cars Sold: %hd | "
-           "Discount Given: %s | Customer Name: %s\n\n",
+    printf("Sale Index: %-3d | Final Sale Amount (after any discount): %-10.2f | Car Model: %-15s | "
+           "Unitary Price of Car: %-10.2f | Number of cars Sold: %-3hd | "
+           "Discount Given: %-3s | Customer Name: %-s\n\n",
            position, price, carModels[modelOfCar],
-           carPrices[modelOfCar], numberOfCarsPerSale[position],
-           discountGivenText, customerNames[position]);
+           carPrices[modelOfCar], sales[position].numberOfCarsPerSale,
+           discountGivenText, sales[position].customerNames);
+
 }
 
 void menu_viewSales()
@@ -483,19 +514,21 @@ void menu_viewSales()
     float totalSalesValue = 0;
     unsigned int carsSold = 0;
 
-    printf("All Sales Data:\n\n");
+    printf("\n                                              <<<All Sales Data>>>\n");
+    printf("--------------------------------------------------------------------------------------------------------------------\n");
 
     for (int i = 0; i < numberOfSales; i++)
     {
 
-        int modelOfCar = carModelPerSale[i];
-        float price = numberOfCarsPerSale[i] * carPrices[modelOfCar];
+        int modelOfCar = sales[i].carModelPerSale;
+        float price = sales[i].numberOfCarsPerSale * carPrices[modelOfCar];
 
         printSalesDataAtPosition(i);
 
         totalSalesValue += price;
-        carsSold += numberOfCarsPerSale[i];
+        carsSold += sales[i].numberOfCarsPerSale;
     }
+    printf("--------------------------------------------------------------------------------------------------------------------\n");
 
     printf("We have sold a total of \n%hd cars valued to be %f GBP. We currently have %hd cars available for sale.\n",
            carsSold, totalSalesValue, carsAvailable);
@@ -509,7 +542,7 @@ void printSalesDataBasedOnCarsSoldBetweenMinMax(unsigned short min, unsigned sho
     for (int i = 0; i < numberOfSales; i++)
     {
 
-        if (min <= numberOfCarsPerSale[i] && numberOfCarsPerSale[i] <= max)
+        if (min <= sales[i].numberOfCarsPerSale && sales[i].numberOfCarsPerSale <= max)
         {
 
             printSalesDataAtPosition(i);
@@ -567,6 +600,10 @@ int main()
 
             case MENU_OPTION_FILTER_SALES_STAT:
                 menu_viewSalesBetween();
+                break;
+
+            case MENU_View_Available_CARS:
+                menu_viewStock();
                 break;
 
             case MENU_OPTION_EXIT_APPLICATION:
